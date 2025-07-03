8126701acf4dd96ba58903c9bfe15fc7e8ce8c8c
abduraufdev0788 <abduraufnasrullayev790@gmail.com>
2025-07-03 20:29:29 +0500

    bajaildi

--- rating_sorter.py
+++ rating_sorter.py
@@ -1,1 +1,18 @@
+import csv
 
+
+with open('students.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile)
+    students = list(reader)
+
+students.sort(key=lambda x : int(x['score']), reverse=True)
+with open('rating.csv', 'w') as csvfile:
+    writer = csv.DictWriter(csvfile ,fieldnames=['rank','name','score'])
+    writer.writeheader()
+
+    i = 1
+    for student in students:
+        student['rank'] = i
+        writer.writerow(student)
+        i += 1
+       
